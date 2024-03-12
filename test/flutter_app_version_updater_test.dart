be553ce5fb8997cf90a7efda053e95e1627b5d89
GurkaranVir Singh <karanbambhrah02@yahoo.com>
2024-03-12 17:18:22 +0530

    :fire: Initial

---
+++ test/flutter_app_version_updater_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_app_version_updater/flutter_app_version_updater.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
